e1a4367dd852765c4c4e4bdaa86090d6740179c4
Alzz hosting <alzzoffchosting@gmail.com>
2025-01-01 15:18:47 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -4,7 +4,7 @@ export const metaData = {
   name: "Sirius",
   ogImage: "/opengraph-image.png",
   description:
-    "A clean, fast, and lightweight portfolio template built with Next.js, Vercel, and Tailwind CSS for optimal performance.",
+    "WEB PORTOFOLIO ALZZOFFC.",
 };
 
 export const socialLinks = {
